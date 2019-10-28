9376b669c206a2c2b1ea4a0d27f45153c6e2d522
oladimeji-ojo <ojokuredim@gmail.com>
2019-10-28 14:54:41 +0000

    created a createNewUser function to post user

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 // implement your API here
 
+const express = require("express");
+const cors = require("cors");
 
+const db = require("./data/db");
 
-const express = require('express')
-const cors = require('cors')
+const app = express();
 
-const db = require('./data/db')
+app.use(cors());
+app.use(express.json());
+
+app.post("/api/users", createNewUser);
+
+function createNewUser(req, res) {
+  const user = {
+    name: req.body.name
+    // body: req.body.bio
+  };
+
+  db.insert(user)
+    .then(data => {
+      res.status(201).json({ succes: true, data });
+    })
+    .catch(error => {
+      res.status(500).json({
+        success: false,
+        error
+      });
+    });
+}
+
+app.listen(process.env.PORT || 8000, () => {
+  console.log("listening on " + (process.env.PORT || 8000));
+});
