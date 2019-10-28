bef376d969061a151901004fa1aa4aa852bb39a5
oladimeji-ojo <ojokuredim@gmail.com>
2019-10-28 17:56:15 +0000

    created a deleteUser function to delete a user

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use(express.json());
 app.post("/api/users", createNewUser);
 app.get("/api/users", getAllUsers);
 app.get("/api/users/:id", getUserById);
+app.get("/api/users/:id", deleteUser);
 
 function createNewUser(req, res) {
   const user = {
@@ -45,7 +46,7 @@ function getAllUsers(req, res) {
     .then(users => {
       res.status(200).json({
         success: true,
-        user
+        users
       });
     })
     .catch(error => {
@@ -76,6 +77,31 @@ function getUserById(req, res) {
         error: "The user information could not be retrieved."
       });
     });
+}
+
+function deleteUser(req, res) {
+  const id = req.params;
+  db.remove(id)
+
+    .then(user => {
+      if (user === 0) {
+        res.status(404).json({
+          success: false,
+          message: "The user with the specified ID does not exist."
+        });
+      } else {
+        res.status(201).json({
+          success: true,
+          user
+        });
+      }
+    })
+    .catch(error =>
+      res.status(500).json({
+        success: false,
+        error: "The user could not be removed"
+      })
+    );
 }
 
 app.listen(process.env.PORT || 8000, () => {
